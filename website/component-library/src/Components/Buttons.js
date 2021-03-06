5e84006b63b2e069a57c2b6294f1ff2e1d61fc90
Rohan Gupta <56235204+rohan9896@users.noreply.github.com>
2021-03-06 14:06:37 +0530

    create react app

---
+++ website/component-library/src/Components/Buttons.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Buttons() {
+    return (
+        <div>
+            <h1>Buttons</h1>
+            <div>
+                <p>Primary Button</p>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Buttons
